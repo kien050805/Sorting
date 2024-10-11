c34dd8de9e613245de9157458a96abae036bed6f
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-10-11 15:17:39 -0400

    init

---
+++ sorting.hpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+void insertion_sort(T *arr, long n);
+
+T* merge_sort(T *arr, long n);
+
+void quicksort(T *arr, long n);
+
+void improved_quicksort(T * arr, long n);
+
+void randomized_quicksort(T *arr, long n);
