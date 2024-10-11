86e590fd73ac8147a52e9103a95a80cb5914bc22
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-10-11 15:28:24 -0400

    temp

--- sorting.hpp
+++ sorting.hpp
@@ -1,11 +1,16 @@
 using namespace std;
 
+template <class T>
 void insertion_sort(T *arr, long n);
 
+template <class T>
 T* merge_sort(T *arr, long n);
 
+template <class T>
 void quicksort(T *arr, long n);
 
+template <class T>
 void improved_quicksort(T * arr, long n);
 
+template <class T>
 void randomized_quicksort(T *arr, long n);
