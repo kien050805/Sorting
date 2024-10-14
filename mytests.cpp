e88475f3e3976f686239f2f0fb1dd20e0b2067cf
MaxTBM <animalsfreeworld@gmail.com>
2024-10-14 17:42:26 -0400

    Added documentation

--- mytests.cpp
+++ mytests.cpp
@@ -18,6 +18,13 @@ using namespace std::chrono;
 const int max_val = INT_MAX;
 const int min_val = INT_MIN;
 
+/*===========================================================================
+insertion_sanity_check()
+Run a series of predefined test cases to ensure that the insertion sort
+algorithm is functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
 bool insertion_sanity_check()
 {
     bool check = true;
@@ -33,6 +40,7 @@ bool insertion_sanity_check()
     double expected_D[5] = {.0, 1.14, 1.618, 2.71, 3.14};
     int expected_E[5] = {0, 0, 0, 0, 0};
 
+    // Test with regular unsorted list
     insertion_sort(A, 9);
     for (int i = 0; i < 9; i++)
     {
@@ -42,8 +50,10 @@ bool insertion_sanity_check()
         };
     };
 
+    // Test with list of no items
     insertion_sort(B, 0);
 
+    // Test with list of strings
     insertion_sort(C, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -53,6 +63,7 @@ bool insertion_sanity_check()
         };
     };
 
+    // Test with list of doubles
     insertion_sort(D, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -62,6 +73,7 @@ bool insertion_sanity_check()
         };
     };
 
+    // Test with list of the same items
     insertion_sort(E, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -73,6 +85,13 @@ bool insertion_sanity_check()
     return check;
 };
 
+/*===========================================================================
+merge_sanity_check()
+Run a series of predefined test cases to ensure that the merge sort
+algorithm is functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
 bool merge_sanity_check()
 {
     bool check = true;
@@ -88,6 +107,7 @@ bool merge_sanity_check()
     double expected_D[5] = {.0, 1.14, 1.618, 2.71, 3.14};
     int expected_E[5] = {0, 0, 0, 0, 0};
 
+    // Test with regular unsorted list
     int *sorted_A = merge_sort(A, 9);
     for (int i = 0; i < 9; i++)
     {
@@ -97,8 +117,10 @@ bool merge_sanity_check()
         };
     };
 
+    // Test with list of no items
     int *sorted_B = merge_sort(B, 0);
 
+    // Test with list of strings
     string *sorted_C = merge_sort(C, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -108,6 +130,7 @@ bool merge_sanity_check()
         };
     };
 
+    // Test with list of doubles
     double *sorted_D = merge_sort(D, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -117,6 +140,7 @@ bool merge_sanity_check()
         };
     };
 
+    // Test with list of the same item
     int *sorted_E = merge_sort(E, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -128,6 +152,13 @@ bool merge_sanity_check()
     return check;
 };
 
+/*===========================================================================
+quick_sanity_check()
+Run a series of predefined test cases to ensure that the quick sort
+algorithm is functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
 bool quick_sanity_check()
 {
     bool check = true;
@@ -143,6 +174,7 @@ bool quick_sanity_check()
     double expected_D[5] = {.0, 1.14, 1.618, 2.71, 3.14};
     int expected_E[5] = {0, 0, 0, 0, 0};
 
+    // Test with regular unsorted list
     quicksort(A, 9);
     for (int i = 0; i < 9; i++)
     {
@@ -152,8 +184,10 @@ bool quick_sanity_check()
         };
     };
 
+    // Test with list of no items
     quicksort(B, 0);
 
+    // Test with list of strings
     quicksort(C, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -163,6 +197,7 @@ bool quick_sanity_check()
         };
     };
 
+    // Test with list of doubles
     quicksort(D, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -172,6 +207,7 @@ bool quick_sanity_check()
         };
     };
 
+    // Test with list of the same item
     quicksort(E, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -183,6 +219,13 @@ bool quick_sanity_check()
     return check;
 };
 
+/*===========================================================================
+ran_quick_sanity_check()
+Run a series of predefined test cases to ensure that the randomized merge sort
+algorithm is functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
 bool ran_quick_sanity_check()
 {
     bool check = true;
@@ -198,6 +241,7 @@ bool ran_quick_sanity_check()
     double expected_D[5] = {.0, 1.14, 1.618, 2.71, 3.14};
     int expected_E[5] = {0, 0, 0, 0, 0};
 
+    // Test with regular unsorted list
     randomized_quicksort(A, 9);
     for (int i = 0; i < 9; i++)
     {
@@ -207,8 +251,10 @@ bool ran_quick_sanity_check()
         };
     };
 
+    // Test with list of no items
     randomized_quicksort(B, 0);
 
+    // Test with list of strings
     randomized_quicksort(C, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -218,6 +264,7 @@ bool ran_quick_sanity_check()
         };
     };
 
+    // Test with list of doubles
     randomized_quicksort(D, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -227,6 +274,7 @@ bool ran_quick_sanity_check()
         };
     };
 
+    // Test with list of the same item
     randomized_quicksort(E, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -239,6 +287,13 @@ bool ran_quick_sanity_check()
     return check;
 };
 
+/*===========================================================================
+improved_merge_sanity_check()
+Run a series of predefined test cases to ensure that the improved merge sort
+algorithm is functioning correctly.
+Parameters: None
+Return: boolean value depending on if the function pass the test cases
+===========================================================================*/
 bool improved_quick_sanity_check()
 {
     bool check = true;
@@ -254,6 +309,7 @@ bool improved_quick_sanity_check()
     double expected_D[5] = {.0, 1.14, 1.618, 2.71, 3.14};
     int expected_E[5] = {0, 0, 0, 0, 0};
 
+    // Test with regular unsorted list
     improved_quicksort(A, 9);
     for (int i = 0; i < 9; i++)
     {
@@ -263,8 +319,10 @@ bool improved_quick_sanity_check()
         };
     };
 
+    // Test with list of items
     improved_quicksort(B, 0);
 
+    // Test with list of strings
     improved_quicksort(C, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -274,6 +332,7 @@ bool improved_quick_sanity_check()
         };
     };
 
+    // Test with list of doubles
     improved_quicksort(D, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -283,6 +342,7 @@ bool improved_quick_sanity_check()
         };
     };
 
+    // Test with list of the same item
     improved_quicksort(E, 5);
     for (int i = 0; i < 5; i++)
     {
@@ -294,6 +354,12 @@ bool improved_quick_sanity_check()
     return check;
 };
 
+/*===========================================================================
+rand_arr()
+Generate an array of random integers of the desired length.
+Parameters: length of the array to be generated
+Return: array of random integers
+===========================================================================*/
 int *rand_arr(long length)
 {
     int *arr = new int[length];
@@ -313,19 +379,12 @@ int *rand_arr(long length)
     return arr;
 };
 
-long long sorting_run_time(long n)
-{
-    // Sort array length n
-    int *arr = rand_arr(n);
-    auto start = high_resolution_clock::now();
-    // _sort(arr, 10)
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    long long run_time = duration.count();
-
-    return run_time;
-};
-
+/*===========================================================================
+insertion_sort_run_time()
+Calculates the run time of insertion sort based on the input length
+Parameters: length of array to be sorted
+Return: the run time of insertion_sort() in microseconds
+===========================================================================*/
 long long insertion_sort_run_time(long n)
 {
     // Sort array length n
@@ -339,6 +398,12 @@ long long insertion_sort_run_time(long n)
     return run_time;
 };
 
+/*===========================================================================
+merge_sort_run_time()
+Calculates the run time of merge sort based on the input length
+Parameters: length of array to be sorted
+Return: the run time of merge_sort() in microseconds
+===========================================================================*/
 long long merge_sort_run_time(long n)
 {
     // Sort array length n
@@ -352,6 +417,12 @@ long long merge_sort_run_time(long n)
     return run_time;
 };
 
+/*===========================================================================
+quick_sort_run_time()
+Calculates the run time of quick sort based on the input length
+Parameters: length of array to be sorted
+Return: the run time of quicksort() in microseconds
+===========================================================================*/
 long long quick_sort_run_time(long n)
 {
     // Sort array length n
@@ -365,6 +436,12 @@ long long quick_sort_run_time(long n)
     return run_time;
 };
 
+/*===========================================================================
+improved_quick_sort_run_time()
+Calculates the run time of improved quick sort based on the input length
+Parameters: length of array to be sorted
+Return: the run time of improved_quicksort() in microseconds
+===========================================================================*/
 long long improved_quick_sort_run_time(long n)
 {
     // Sort array length n
@@ -378,6 +455,12 @@ long long improved_quick_sort_run_time(long n)
     return run_time;
 };
 
+/*===========================================================================
+ran_quick_sort_run_time()
+Calculates the run time of randomized quick sort based on the input length
+Parameters: length of array to be sorted
+Return: the run time of randomized_quicksort() in microseconds
+===========================================================================*/
 long long ran_quick_sort_run_time(long n)
 {
     // Sort array length n
@@ -391,6 +474,12 @@ long long ran_quick_sort_run_time(long n)
     return run_time;
 };
 
+/*===========================================================================
+cpp_sort_run_time()
+Calculates the run time of C++ sort function based on the input length
+Parameters: length of array to be sorted
+Return: the run time of sort() in microseconds
+===========================================================================*/
 long long cpp_sort_run_time(long n)
 {
     // Sort array length n
@@ -404,6 +493,14 @@ long long cpp_sort_run_time(long n)
     return run_time;
 };
 
+/*===========================================================================
+all_run_time()
+Runs all of the run time functions over arrays of size 10, 100, 1000, 10000, 
+100000, and calculates the average run time of 10 times for each sorting 
+algorithm and then print to terminal.
+Parameters: None
+Return: None
+===========================================================================*/
 void all_run_time()
 {
     // Array for testing array lengths
@@ -530,7 +627,11 @@ void all_run_time()
     cout << endl;
 };
 
-
+/*===========================================================================
+main()
+Run and throw exceptions for all of the sanity check functions and run 
+all_run_time()
+===========================================================================*/
 int main()
 {
     int count = 0;
@@ -583,5 +684,5 @@ int main()
     };
     cout << "All test cases passed!" << endl;
 
-    // all_run_time();
+    all_run_time();
 };
